0446702018ebcdfa691aef8eb30ed7717ac21d65
KTheES <keunsu2889@gmail.com>
2025-03-28 20:59:44 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import './App.css'
+
+
+function App() {
+
+  return(
+    <Router>
+      <Routes>
+      {/* 루트 경로 */}
+      <Route path="/" element={<Home />} />
+      
+      {/* 영화 상세 정보 */}
+      <Route path="/movie/:id" element={<Detail />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/App2.jsx
@@ -0,0 +1,42 @@
+
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  // async - await
+  const getMovies = async() => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  } // -> 이걸 useEffect에 넣어서 렌더링 될 때 한 번만 getMovies라는 함수를 실행시키겠다.
+  // 나는 처음에 컴포넌트를 실행할 때 {} 안에 있는 코드를 한 번만 실행시키고 싶다.
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  // console.log(movies);  // 장르 받아올건데 장르가 배열로 되어있습니다.
+
+  return (
+    <div>
+      {loading ? <h1 >로딩 중 . . .🐢</h1> : <div>{movies.map(movie => 
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((genre) => (<li key={genre}>{genre}</li>))} 
+          </ul>
+        </div>
+      )}</div>
+      }
+    </div>
+  )
+}
+
+export default App
